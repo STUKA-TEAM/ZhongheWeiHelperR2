e46717375b0962198220ec0a6423dbd347abbd7e
believeit <374344282@qq.com>
2013-12-19 00:08:34 +0800

    weixin 交互

--- src/controller/customer/EloveController.java
+++ src/controller/customer/EloveController.java
@@ -6,27 +6,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EloveController {
-@RequestMapping(value = "/story", method = RequestMethod.GET)
+@RequestMapping(value = "/elove/story", method = RequestMethod.GET)
 public String story(){
 	return "EloveViews/story";
 }
-@RequestMapping(value = "/wedding", method = RequestMethod.GET)
+@RequestMapping(value = "/elove/wedding", method = RequestMethod.GET)
 public String wedding(){
 	return "EloveViews/wedding";
 }
-@RequestMapping(value = "/dress", method = RequestMethod.GET)
+@RequestMapping(value = "/elove/dress", method = RequestMethod.GET)
 public String dress(){
 	return "EloveViews/dress";
 }
-@RequestMapping(value = "/record", method = RequestMethod.GET)
+@RequestMapping(value = "/elove/record", method = RequestMethod.GET)
 public String record(){
 	return "EloveViews/record";
 }
-@RequestMapping(value = "/wishmessage", method = RequestMethod.GET)
+@RequestMapping(value = "/elove/wishmessage", method = RequestMethod.GET)
 public String wishMessage(){
 	return "EloveViews/wishMessage";
 }
-@RequestMapping(value = "/joinmessage", method = RequestMethod.GET)
+@RequestMapping(value = "/elove/joinmessage", method = RequestMethod.GET)
 public String joinMessage(){
 	return "EloveViews/joinMessage";
 }
--- src/tools/MethodUtils.java
+++ src/tools/MethodUtils.java
@@ -7,11 +7,11 @@ import java.util.Properties;
 public class MethodUtils {
 public static String getApplicationPath(){
 	String applicationPath = "";	
-	InputStream inputStream = MethodUtils.class.getResourceAsStream("environment.properties");
+	InputStream inputStream = MethodUtils.class.getResourceAsStream("/environment.properties");
 	Properties properties = new Properties();
 	try {
 		properties.load(inputStream);
-		applicationPath = (String) properties.get("host");
+		applicationPath = (String) properties.get("applicationPath");
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
