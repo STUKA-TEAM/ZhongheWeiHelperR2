342b21c8a1f841c14a3809b64a70502dfdc82c7e
believeit <374344282@qq.com>
2014-01-15 22:07:10 +0800

    完成feedback

--- src/controller/customer/EloveController.java
+++ src/controller/customer/EloveController.java
@@ -84,7 +84,7 @@ public String addWishMessage(@RequestBody String json){
 	eloveMessage.setCreateTime(current);
 	int result = eloveInteractDAO.insertMessage(eloveMessage);
 	ResponseMessage message = new ResponseMessage();
-	if(result==1){
+	if(result > 0){
 		message.setStatus(true);
 		message.setMessage("发送成功！");
 	}else{
@@ -109,7 +109,7 @@ public String addJoinMessage(@RequestBody String json){
 	int result = eloveInteractDAO.insertJoinInfo(eloveJoinInfo);
 	ResponseMessage message = new ResponseMessage();
 	
-	if(result==1){
+	if(result > 0){
 		message.setStatus(true);
 		message.setMessage("发送成功！");
 	}else{
