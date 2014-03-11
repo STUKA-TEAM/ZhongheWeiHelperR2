5951511c75ed8890d52b01c1c375d5abd163150f
Ben Youcheng <youcheng.ben@sap.com>
2014-03-11 23:48:34 +0800

    add lottery module

--- src/elove/EloveJoinInfo.java
+++ src/elove/EloveJoinInfo.java
@@ -4,46 +4,58 @@ import java.sql.Timestamp;
 
 
 public class EloveJoinInfo {
-private int id;
-private int eloveid;
-private Timestamp createTime;
-private String name;
-private String phone;
-private int number;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public int getEloveid() {
-	return eloveid;
-}
-public void setEloveid(int eloveid) {
-	this.eloveid = eloveid;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getPhone() {
-	return phone;
-}
-public void setPhone(String phone) {
-	this.phone = phone;
-}
-public int getNumber() {
-	return number;
-}
-public void setNumber(int number) {
-	this.number = number;
-}
-public Timestamp getCreateTime() {
-	return createTime;
-}
-public void setCreateTime(Timestamp createTime) {
-	this.createTime = createTime;
-}
+	private int id;
+	private int eloveid;
+	private Timestamp createTime;
+	private String name;
+	private String phone;
+	private int number;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getEloveid() {
+		return eloveid;
+	}
+
+	public void setEloveid(int eloveid) {
+		this.eloveid = eloveid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
 }
--- src/elove/EloveMessage.java
+++ src/elove/EloveMessage.java
@@ -2,40 +2,57 @@ package elove;
 
 import java.sql.Timestamp;
 
+/**
+ * @Title: EloveMessage
+ * @Description: elove祝福语
+ * @Company: ZhongHe
+ * @author ben
+ * @date 2014年3月11日
+ */
 public class EloveMessage {
-private int id;
-private int eloveid;
-private Timestamp createTime;
-private String name;
-private String content;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public int getEloveid() {
-	return eloveid;
-}
-public void setEloveid(int eloveid) {
-	this.eloveid = eloveid;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getContent() {
-	return content;
-}
-public void setContent(String content) {
-	this.content = content;
-}
-public Timestamp getCreateTime() {
-	return createTime;
-}
-public void setCreateTime(Timestamp createTime) {
-	this.createTime = createTime;
-}
+	private int id;
+	private int eloveid;
+	private Timestamp createTime;
+	private String name;
+	private String content;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getEloveid() {
+		return eloveid;
+	}
+
+	public void setEloveid(int eloveid) {
+		this.eloveid = eloveid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
 }
